4fe06cae2c085aee9fe93d70dea976c5730f92d4
Adam Burski <burskiadamwork@gmail.com>
2024-05-08 19:50:05 +0200

    Create interface IPatientDao

---
+++ src/main/java/com/capgemini/wsb/persistence/dao/interfaces/IPatientDao.java
@@ -0,0 +1,6 @@
+package com.capgemini.wsb.persistence.dao.interfaces;
+
+import com.capgemini.wsb.persistence.entity.PatientEntity;
+
+public interface IPatientDao extends IDao<PatientEntity, Long>{
+}
