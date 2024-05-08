14c7fb048a00cb5b90fff0a2cbece738913a2299
Adam Burski <burskiadamwork@gmail.com>
2024-05-08 18:47:49 +0200

    Create data transport object for Medical Treatment

---
+++ src/main/java/com/capgemini/wsb/dto/MedicalTreatmentDto.java
@@ -0,0 +1,33 @@
+package com.capgemini.wsb.dto;
+
+import com.capgemini.wsb.persistence.enums.TreatmentType;
+
+public class MedicalTreatmentDto {
+    private int id;
+    private String description;
+    private TreatmentType type;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public TreatmentType getType() {
+        return type;
+    }
+
+    public void setType(TreatmentType type) {
+        this.type = type;
+    }
+}
