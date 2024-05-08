0cf8ae4be8970e402b25e28a596e5816948bed69
Adam Burski <burskiadamwork@gmail.com>
2024-05-08 21:04:19 +0200

    Create DoctorDao

---
+++ src/main/java/com/capgemini/wsb/persistence/dao/impl/DoctorDao.java
@@ -0,0 +1,7 @@
+package com.capgemini.wsb.persistence.dao.impl;
+
+import com.capgemini.wsb.persistence.dao.interfaces.IDoctorDao;
+import com.capgemini.wsb.persistence.entity.DoctorEntity;
+
+public class DoctorDao extends AbstractDao<DoctorEntity,Long> implements IDoctorDao {
+}
