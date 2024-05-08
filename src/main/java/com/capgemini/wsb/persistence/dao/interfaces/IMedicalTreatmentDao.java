34c080b137a5abe4905d5b17808dea5feb80f66a
Adam Burski <burskiadamwork@gmail.com>
2024-05-08 21:01:12 +0200

    Create interface IMedicalTreatment

---
+++ src/main/java/com/capgemini/wsb/persistence/dao/interfaces/IMedicalTreatmentDao.java
@@ -0,0 +1,6 @@
+package com.capgemini.wsb.persistence.dao.interfaces;
+
+import com.capgemini.wsb.persistence.entity.MedicalTreatmentEntity;
+
+public interface IMedicalTreatmentDao extends IDao<MedicalTreatmentEntity,Long> {
+}
