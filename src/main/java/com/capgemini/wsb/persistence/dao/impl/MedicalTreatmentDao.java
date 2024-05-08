8fd37660c8452fab67774c89bc8cfe6c6a692d68
Adam Burski <burskiadamwork@gmail.com>
2024-05-08 21:06:55 +0200

    Create MedicalTreatmentDao

---
+++ src/main/java/com/capgemini/wsb/persistence/dao/impl/MedicalTreatmentDao.java
@@ -0,0 +1,7 @@
+package com.capgemini.wsb.persistence.dao.impl;
+
+import com.capgemini.wsb.persistence.dao.interfaces.IMedicalTreatmentDao;
+import com.capgemini.wsb.persistence.entity.MedicalTreatmentEntity;
+
+public class MedicalTreatmentDao extends AbstractDao<MedicalTreatmentEntity, Long> implements IMedicalTreatmentDao {
+}
