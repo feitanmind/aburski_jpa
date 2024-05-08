85905651b99a18198db586a445507f3af6b1aadf
Adam Burski <burskiadamwork@gmail.com>
2024-05-08 21:07:49 +0200

    Create PatientDao

---
+++ src/main/java/com/capgemini/wsb/persistence/dao/impl/PatientDao.java
@@ -0,0 +1,7 @@
+package com.capgemini.wsb.persistence.dao.impl;
+
+import com.capgemini.wsb.persistence.dao.interfaces.IPatientDao;
+import com.capgemini.wsb.persistence.entity.PatientEntity;
+
+public class PatientDao extends AbstractDao<PatientEntity,Long> implements IPatientDao {
+}
