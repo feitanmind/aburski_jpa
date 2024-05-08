63357aa314f0f6123e5669dfcd5f8988368c3747
Adam Burski <burskiadamwork@gmail.com>
2024-05-08 21:09:37 +0200

    Create VisitDao

---
+++ src/main/java/com/capgemini/wsb/persistence/dao/impl/VisitDao.java
@@ -0,0 +1,7 @@
+package com.capgemini.wsb.persistence.dao.impl;
+
+import com.capgemini.wsb.persistence.dao.interfaces.IVisitDao;
+import com.capgemini.wsb.persistence.entity.VisitEntity;
+
+public class VisitDao extends AbstractDao<VisitEntity,Long> implements IVisitDao {
+}
