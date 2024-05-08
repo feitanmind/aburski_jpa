a3b18defc13b3a112b9ad9591f94ab62c9bbb109
Adam Burski <burskiadamwork@gmail.com>
2024-05-08 21:40:04 +0200

    Correct type

--- src/main/java/com/capgemini/wsb/service/interfaces/ICrudService.java
+++ src/main/java/com/capgemini/wsb/service/interfaces/ICrudService.java
@@ -5,7 +5,7 @@ import com.capgemini.wsb.mapper.AbstractMapper;
 import java.io.Serializable;
 
 public interface ICrudService<D,I extends Serializable,E> {
-    D crete(D dto, AbstractMapper<D,E> mapper);
+    D create(D dto, AbstractMapper<D,E> mapper);
     D get(I id, AbstractMapper<E,D> mapper);
     D update(D dto, AbstractMapper<D,E> mapper);
     void delete(I id);
