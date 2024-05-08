b6648b98b1c100c127bcabf4abd557d1043fc993
Adam Burski <burskiadamwork@gmail.com>
2024-05-08 19:50:53 +0200

    Create interface IDoctorDao

---
+++ src/main/java/com/capgemini/wsb/persistence/dao/interfaces/IDoctorDao.java
@@ -0,0 +1,6 @@
+package com.capgemini.wsb.persistence.dao.interfaces;
+
+import com.capgemini.wsb.persistence.entity.DoctorEntity;
+
+public interface IDoctorDao extends IDao<DoctorEntity,Long> {
+}
