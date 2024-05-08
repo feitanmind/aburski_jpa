75392731c2584f5ee859f9d401fb816d8cac3668
Adam Burski <burskiadamwork@gmail.com>
2024-05-08 21:25:52 +0200

    Fix interface ICrudService

--- src/main/java/com/capgemini/wsb/service/interfaces/ICrudService.java
+++ src/main/java/com/capgemini/wsb/service/interfaces/ICrudService.java
@@ -2,7 +2,7 @@ package com.capgemini.wsb.service.interfaces;
 
 import java.io.Serializable;
 
-public interface ICrudService<D,I,M extends Serializable> {
+public interface ICrudService<D,I extends Serializable, M> {
     D crete(D dto, M mapper);
     D get(I id, M mapper);
     D update(D dto, M mapper);
