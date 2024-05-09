5b19eefb0a5df4b8bfd8bc88d7bea2a358781182
Adam Burski <burskiadamwork@gmail.com>
2024-05-09 23:06:25 +0200

    Create interfae IVisitService

---
+++ src/main/java/com/capgemini/wsb/service/interfaces/IVisitService.java
@@ -0,0 +1,13 @@
+package com.capgemini.wsb.service.interfaces;
+
+import com.capgemini.wsb.dto.VisitDto;
+
+import java.util.List;
+
+public interface IVisitService {
+    VisitDto get(Long id);
+    VisitDto create(VisitDto dto);
+    VisitDto update(VisitDto dto);
+    void delete(Long id);
+    List<VisitDto> getList();
+}
