7380411bd32d6f56df188031a3892cf23f90b0dc
Adam Burski <burskiadamwork@gmail.com>
2024-05-13 22:30:00 +0200

    Add methods to repository (dao) from excercise

--- src/main/java/com/capgemini/wsb/persistence/dao/impl/PatientDao.java
+++ src/main/java/com/capgemini/wsb/persistence/dao/impl/PatientDao.java
@@ -1,9 +1,44 @@
 package com.capgemini.wsb.persistence.dao.impl;
 
+import com.capgemini.wsb.dto.VisitDto;
 import com.capgemini.wsb.persistence.dao.interfaces.IPatientDao;
 import com.capgemini.wsb.persistence.entity.PatientEntity;
+import com.capgemini.wsb.persistence.entity.VisitEntity;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class PatientDao extends AbstractDao<PatientEntity,Long> implements IPatientDao {
+    @Override
+    public List<PatientEntity> getAllPatientsWithSpecifiedLastname(String lastName) {
+        return entityManager
+                .createQuery("SELECT p FROM PatientEntity p WHERE p.lastName == :ln",PatientEntity.class)
+                .setParameter("ln",lastName)
+                .getResultList();
+    }
+
+    @Override
+    public List<VisitEntity> getAllVisitAssignedToPatient(Long patientId) {
+        return entityManager
+                .createQuery("SELECT p.visits FROM PatientEntity p WHERE p.id = :pid", VisitEntity.class)
+                .setParameter("pid",patientId)
+                .getResultList();
+    }
+
+    @Override
+    public List<PatientEntity> getAllPatientWhoHaveMoreThanXVisits(int numberOfVisits) {
+        return entityManager
+                .createQuery("SELECT p FROM PatientEntity p WHERE p.visits.size > :nov",PatientEntity.class)
+                .setParameter("nov",numberOfVisits)
+                .getResultList();
+    }
+
+    @Override
+    public List<PatientEntity> getAllPatientWhoHaveSmallerPeselNumber(Long pesel) {
+        return entityManager
+                .createQuery("SELECT p FROM PatientEntity p WHERE p.peselNumber < :pn",PatientEntity.class)
+                .setParameter("pn",pesel)
+                .getResultList();
+    }
 }
--- src/main/java/com/capgemini/wsb/persistence/dao/interfaces/IPatientDao.java
+++ src/main/java/com/capgemini/wsb/persistence/dao/interfaces/IPatientDao.java
@@ -1,6 +1,37 @@
 package com.capgemini.wsb.persistence.dao.interfaces;
 
+import com.capgemini.wsb.dto.VisitDto;
 import com.capgemini.wsb.persistence.entity.PatientEntity;
+import com.capgemini.wsb.persistence.entity.VisitEntity;
+
+import java.util.List;
 
 public interface IPatientDao extends IDao<PatientEntity, Long>{
+    /**
+     * Get List of Patient Entity with specified LastName
+     * @param lastName
+     * @return List(PatientEntity)
+     */
+    List<PatientEntity> getAllPatientsWithSpecifiedLastname(String lastName);
+
+    /**
+     * Get List Of Visits Assigned to patient with passed id
+     * @param patientId
+     * @return List of VisitEntity
+     */
+    List<VisitEntity> getAllVisitAssignedToPatient(Long patientId);
+
+    /**
+     * Get List of Patients With more than passed number of visits
+     * @param numberOfVisits
+     * @return List of PatientEntity
+     */
+    List<PatientEntity> getAllPatientWhoHaveMoreThanXVisits(int numberOfVisits);
+
+    /**
+     * Get List of Patients with smaller Pesel Number
+     * @param pesel
+     * @return List of PatientEntity
+     */
+    List<PatientEntity> getAllPatientWhoHaveSmallerPeselNumber(Long pesel);
 }
